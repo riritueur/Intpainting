03cf7b33ba66c923040a8f0132c72a3ddf610d82
Richard Peres <riritueur@gmail.com>
2017-12-06 13:44:05 +0100

    Dark theme is here

--- Int_Pingu/src/topologyV2/InpaintingJFrame.java
+++ Int_Pingu/src/topologyV2/InpaintingJFrame.java
@@ -1,4 +1,5 @@
 package topologyV2;
+
 import java.awt.Color;
 import java.awt.EventQueue;
 
@@ -14,6 +15,7 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JComponent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -26,25 +28,61 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.JSeparator;
 import javax.swing.border.MatteBorder;
 import javax.swing.border.BevelBorder;
 import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import java.awt.SystemColor;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.TitledBorder;
 
 public class InpaintingJFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel contentPane;
 	private JTextField txtLienImage;
 	private JTextField txtLienMasque;
 	private JTextField txtR;
 	private JTextField txtG;
 	private JTextField txtB;
-	private boolean r=true, g=true ,b=true;
+	private boolean r = true, g = true, b = true;
 
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+	private JButton btnApply;
+
+	private JButton btnColorPicker;
+
+	private JButton btnExec;
+
+	private JComponent lblCouleur;
+
+	private JLabel lblTitreCouleur;
+
+	private JSeparator separator;
+
+	private JLabel masque;
+
+	private JLabel image;
+
+	private JButton btnSearchMasque;
+
+	private JLabel lblMasque;
+
+	private JLabel lblImage;
+
+	private JButton btnSearchImage;
+
+	private JLabel lblTitreImages;
+
+	private JLabel lblLoading;
+
+	private JLabel lblTitre;
+
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException, UnsupportedLookAndFeelException {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -58,155 +96,201 @@ public class InpaintingJFrame extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public InpaintingJFrame() {
+		setBackground(Color.GRAY);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Inpainting");
 		setLocationRelativeTo(null);
-		setBounds(100, 100, 960, 432);
+		setBounds(100, 100, 960, 521);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblTitre = new JLabel("Inpainting");
-		lblTitre.setFont(new Font("Tahoma", Font.BOLD, 30));
-		lblTitre.setBounds(395, 7, 170, 49);
+
+		lblTitre = new JLabel("Inpainting");
+		lblTitre.setForeground(new Color(210, 105, 30));
+		lblTitre.setBackground(Color.WHITE);
+		lblTitre.setFont(new Font("Tahoma", Font.BOLD, 35));
+		lblTitre.setBounds(358, 11, 199, 62);
 		contentPane.add(lblTitre);
-		
-		JLabel lblTitreImages = new JLabel("Sélection des 2 images");
-		lblTitreImages.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblTitreImages.setBounds(65, 35, 191, 22);
+
+		lblLoading = new JLabel();
+		lblLoading.setFont(new Font("Tahoma", Font.PLAIN, 34));
+		lblLoading.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLoading.setForeground(new Color(255, 255, 255));
+		lblLoading.setFocusable(false);
+		lblLoading.setVisible(false);
+		lblLoading.setIcon(new ImageIcon("./Image/loading.gif"));
+		lblLoading.setBounds(388, 183, 96, 93);
+		contentPane.add(lblLoading);
+
+		lblTitreImages = new JLabel("Sélection des 2 images");
+		lblTitreImages.setForeground(new Color(210, 105, 30));
+		lblTitreImages.setBackground(Color.WHITE);
+		lblTitreImages.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblTitreImages.setBounds(42, 99, 243, 30);
 		contentPane.add(lblTitreImages);
-		
-		JButton btnSearchImage = new JButton("Browse");
-		btnSearchImage.setBounds(236, 96, 86, 30);
+
+		btnSearchImage = new JButton("Browse");
+		btnSearchImage.setBackground(SystemColor.textInactiveText);
+		btnSearchImage.setBounds(242, 169, 86, 30);
 		contentPane.add(btnSearchImage);
-		
+
 		txtLienImage = new JTextField();
+		txtLienImage.setHorizontalAlignment(SwingConstants.RIGHT);
+		txtLienImage.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(128, 128, 128)));
+		txtLienImage.setForeground(new Color(192, 192, 192));
+		txtLienImage.setBackground(null);
 		txtLienImage.setFocusable(false);
 		txtLienImage.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		txtLienImage.setColumns(10);
-		txtLienImage.setBounds(25, 96, 206, 30);
+		txtLienImage.setBounds(31, 169, 206, 30);
 		contentPane.add(txtLienImage);
-		
-		JLabel lblImage = new JLabel("Image");
+
+		lblImage = new JLabel("Image");
+		lblImage.setForeground(new Color(210, 105, 30));
+		lblImage.setBackground(Color.WHITE);
 		lblImage.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblImage.setBounds(25, 70, 53, 22);
+		lblImage.setBounds(31, 143, 53, 22);
 		contentPane.add(lblImage);
-		
+
 		txtLienMasque = new JTextField();
+		txtLienMasque.setHorizontalAlignment(SwingConstants.RIGHT);
+		txtLienMasque.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(128, 128, 128)));
+		txtLienMasque.setForeground(new Color(192, 192, 192));
+		txtLienMasque.setBackground(null);
 		txtLienMasque.setFocusable(false);
 		txtLienMasque.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		txtLienMasque.setColumns(10);
-		txtLienMasque.setBounds(25, 183, 206, 30);
+		txtLienMasque.setBounds(31, 256, 206, 30);
 		contentPane.add(txtLienMasque);
-		
-		JLabel lblMasque = new JLabel("Masque");
+
+		lblMasque = new JLabel("Masque");
+		lblMasque.setForeground(new Color(210, 105, 30));
+		lblMasque.setBackground(Color.WHITE);
 		lblMasque.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblMasque.setBounds(25, 157, 53, 22);
+		lblMasque.setBounds(31, 230, 53, 22);
 		contentPane.add(lblMasque);
-		
-		JButton btnSearchMasque = new JButton("Browse");
-		btnSearchMasque.setBounds(236, 183, 86, 30);
+
+		btnSearchMasque = new JButton("Browse");
+		btnSearchMasque.setBackground(SystemColor.textInactiveText);
+		btnSearchMasque.setBounds(242, 256, 86, 30);
 		contentPane.add(btnSearchMasque);
-		
-		JLabel image = new JLabel();
-		image.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.LIGHT_GRAY));
+
+		image = new JLabel();
+		image.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(105, 105, 105)));
 		image.setOpaque(true);
-		image.setBounds(352, 70, 250, 250);
+		image.setBounds(388, 143, 250, 250);
 		image.setBackground(null);
 		contentPane.add(image);
-		
-		JLabel masque = new JLabel();
-		masque.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.LIGHT_GRAY));
+
+		masque = new JLabel();
+		masque.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(105, 105, 105)));
 		masque.setOpaque(true);
-		masque.setBounds(662, 70, 250, 250);
+		masque.setBounds(682, 143, 250, 250);
 		masque.setBackground(null);
 		contentPane.add(masque);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 236, 329, 2);
+
+		separator = new JSeparator();
+		separator.setBackground(new Color(192, 192, 192));
+		separator.setBounds(10, 310, 329, 1);
 		contentPane.add(separator);
-		
-		JLabel lblTitreCouleur = new JLabel("Couleur du Masque");
-		lblTitreCouleur.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblTitreCouleur.setBounds(73, 249, 161, 22);
+
+		lblTitreCouleur = new JLabel("Couleur du Masque");
+		lblTitreCouleur.setForeground(new Color(210, 105, 30));
+		lblTitreCouleur.setBackground(Color.WHITE);
+		lblTitreCouleur.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblTitreCouleur.setBounds(62, 322, 214, 38);
 		contentPane.add(lblTitreCouleur);
-		
-		JLabel lblCouleur = new JLabel();
-		lblCouleur.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.LIGHT_GRAY, Color.LIGHT_GRAY, Color.LIGHT_GRAY, Color.LIGHT_GRAY));
+
+		lblCouleur = new JLabel();
+		lblCouleur.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.LIGHT_GRAY, Color.LIGHT_GRAY, Color.LIGHT_GRAY,
+				Color.LIGHT_GRAY));
 		lblCouleur.setBackground(Color.BLACK);
-		lblCouleur.setBounds(253, 298, 65, 62);
+		lblCouleur.setBounds(263, 371, 65, 62);
 		lblCouleur.setOpaque(true);
 		contentPane.add(lblCouleur);
-		
-		JButton btnExec = new JButton("Restaurer");
+
+		btnExec = new JButton("Restaurer");
+		btnExec.setBackground(new Color(255, 255, 255));
 		btnExec.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnExec.setBounds(514, 346, 228, 39);
+		btnExec.setBounds(518, 419, 228, 39);
 		contentPane.add(btnExec);
-		
-		JButton btnColorPicker = new JButton("Color Picker");
+
+		btnColorPicker = new JButton("Color Picker");
+		btnColorPicker.setBackground(SystemColor.textInactiveText);
 		btnColorPicker.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnColorPicker.setBounds(46, 298, 158, 39);
+		btnColorPicker.setBounds(52, 371, 158, 39);
 		contentPane.add(btnColorPicker);
-		
+
 		txtR = new JTextField();
-		txtR.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtR.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		txtR.setSelectionColor(new Color(184, 134, 11));
+		txtR.setForeground(new Color(255, 0, 0));
+		txtR.setBackground(new Color(192, 192, 192));
+		txtR.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtR.setHorizontalAlignment(SwingConstants.CENTER);
 		txtR.setText("255");
-		txtR.setBounds(31, 355, 40, 30);
+		txtR.setBounds(37, 428, 40, 30);
 		contentPane.add(txtR);
 		txtR.setColumns(10);
-		
+
 		txtG = new JTextField();
-		txtG.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtG.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		txtG.setSelectionColor(new Color(184, 134, 11));
+		txtG.setForeground(new Color(0, 128, 0));
+		txtG.setBackground(new Color(192, 192, 192));
+		txtG.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtG.setHorizontalAlignment(SwingConstants.CENTER);
 		txtG.setText("255");
 		txtG.setColumns(10);
-		txtG.setBounds(76, 355, 40, 30);
+		txtG.setBounds(82, 428, 40, 30);
 		contentPane.add(txtG);
-		
+
 		txtB = new JTextField();
-		txtB.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtB.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		txtB.setSelectionColor(new Color(184, 134, 11));
+		txtB.setForeground(new Color(0, 0, 255));
+		txtB.setBackground(new Color(192, 192, 192));
+		txtB.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtB.setHorizontalAlignment(SwingConstants.CENTER);
 		txtB.setText("255");
 		txtB.setColumns(10);
-		txtB.setBounds(126, 354, 40, 30);
+		txtB.setBounds(132, 427, 40, 30);
 		contentPane.add(txtB);
-		
-		JButton btnApply = new JButton("=>");
-		btnApply.setBounds(182, 354, 49, 30);
+
+		btnApply = new JButton("Apply");
+		btnApply.setBackground(SystemColor.textInactiveText);
+		btnApply.setBounds(182, 427, 65, 30);
 		contentPane.add(btnApply);
-		
-		
+
 		btnSearchImage.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				JFileChooser chooser = new JFileChooser("./Image");
 				chooser.setAcceptAllFileFilterUsed(false);
 				FileFilter imagesFilter = new FileNameExtensionFilter("Image", "bmp");
 				chooser.setDialogTitle("Choisir une image");
 				chooser.addChoosableFileFilter(imagesFilter);
-				
+
 				int returnVal = chooser.showOpenDialog(null);
 				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					//File image1 = new File(chooser.getSelectedFile().getPath());
 					try {
 						Image img = ImageIO.read(chooser.getSelectedFile());
-						int x=0, y=0;
-						
-						if(img.getHeight(null)<img.getWidth(null)) {
-							x=250; y = 180;
+						int x = 0, y = 0;
+
+						if (img.getHeight(null) < img.getWidth(null)) {
+							x = 250;
+							y = 180;
+						} else {
+							x = 180;
+							y = 250;
 						}
-						else {
-							x=180; y = 250;
-						}
-						Image newimg = img.getScaledInstance(x, y,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-						image.setBounds(371, 71, x, y);
+						Image newimg = img.getScaledInstance(x, y, java.awt.Image.SCALE_SMOOTH); // scale it the smooth
+																									// way
+						image.setBounds(388, 143, x, y);
 						image.setIcon(new ImageIcon(newimg));
 						image.setVisible(true);
 						txtLienImage.setText(chooser.getSelectedFile().getPath());
@@ -218,28 +302,29 @@ public class InpaintingJFrame extends JFrame {
 		});
 		btnSearchMasque.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				JFileChooser chooser = new JFileChooser("./Image");
 				chooser.setAcceptAllFileFilterUsed(false);
 				FileFilter imagesFilter = new FileNameExtensionFilter("Image", "bmp"); // TODO
 				chooser.setDialogTitle("Choisir une image");
 				chooser.addChoosableFileFilter(imagesFilter);
-				
+
 				int returnVal = chooser.showOpenDialog(null);
 				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					//File image1 = new File(chooser.getSelectedFile().getPath());
 					try {
 						Image img = ImageIO.read(chooser.getSelectedFile());
-						int x=0, y=0;
-						
-						if(img.getHeight(null)<img.getWidth(null)) {
-							x=250; y = 180;
+						int x = 0, y = 0;
+
+						if (img.getHeight(null) < img.getWidth(null)) {
+							x = 250;
+							y = 180;
+						} else {
+							x = 180;
+							y = 250;
 						}
-						else {
-							x=180; y = 250;
-						}
-						Image newimg = img.getScaledInstance(x, y,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-						masque.setBounds(670, 71, x, y);
+						Image newimg = img.getScaledInstance(x, y, java.awt.Image.SCALE_SMOOTH); // scale it the smooth
+																									// way
+						masque.setBounds(682, 143, x, y);
 						masque.setIcon(new ImageIcon(newimg));
 						masque.setVisible(true);
 						txtLienMasque.setText(chooser.getSelectedFile().getPath());
@@ -257,36 +342,29 @@ public class InpaintingJFrame extends JFrame {
 		});
 		btnApply.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				lblCouleur.setBackground(new Color(Integer.parseInt(txtR.getText()), Integer.parseInt(txtG.getText()), Integer.parseInt(txtB.getText())));
+				lblCouleur.setBackground(new Color(Integer.parseInt(txtR.getText()), Integer.parseInt(txtG.getText()),
+						Integer.parseInt(txtB.getText())));
 			}
 		});
 		btnExec.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try{
-					if(!txtLienImage.getText().isEmpty() || !txtLienMasque.getText().isEmpty()) {
-						Matrix matrix = new Matrix(txtLienImage.getText());
-						Matrix matrix2 = new Matrix(txtLienMasque.getText());
-						topologyV2.Color color = new topologyV2.Color(lblCouleur.getBackground().getBlue(),lblCouleur.getBackground().getGreen(),lblCouleur.getBackground().getRed());
-						Mask mask = new Mask(matrix2,color);
-						Inpainting inpainting = new Inpainting(matrix,mask);
-						inpainting.restore(5, 5);
-						BufferedImage img = new BufferedImage(inpainting.image.width, inpainting.image.height,BufferedImage.TYPE_3BYTE_BGR);
-						byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
-						int k=0;
-						for(int j=0;j<matrix.height;j++)
-							for(int i=0;i<matrix.width;i++)
-								for(int c=0;c<3;c++)
-									pixels[k++]=matrix.val[i][j].val[c];
-						JFrame result = new ResultJFrame(img);
+				try {
+					if (!txtLienImage.getText().isEmpty() || !txtLienMasque.getText().isEmpty()) {
+						
+						setAllFalse();
+						lblLoading.setVisible(true);
+
+						JFrame result = new ResultJFrame(doTheIntpainting());
 						result.setVisible(true);
-					}
-					else JOptionPane.showMessageDialog(that, "Erreur : Image et/ou masque incorrect", "Erreur de traitement", 0);
-				}catch(Exception exce){
+					} else
+						JOptionPane.showMessageDialog(that, "Erreur : Image et/ou masque incorrect",
+								"Erreur de traitement", 0);
+				} catch (Exception exce) {
 					exce.printStackTrace();
 				}
 			}
 		});
-		
+
 		txtR.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
@@ -295,14 +373,24 @@ public class InpaintingJFrame extends JFrame {
 					r = false;
 				}
 			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if (txtR.getText().isEmpty())
+					txtR.setText("0");
+			}
 		});
 		txtG.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				if (txtG.getText().equals("255") && g) {
 					txtG.setText("");
-					g=false;
+					g = false;
 				}
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if (txtG.getText().isEmpty())
+					txtG.setText("0");
 			}
 		});
 		txtB.addMouseListener(new MouseAdapter() {
@@ -310,12 +398,81 @@ public class InpaintingJFrame extends JFrame {
 			public void mousePressed(MouseEvent e) {
 				if (txtB.getText().equals("255") && b) {
 					txtB.setText("");
-					b=false;
+					b = false;
 				}
-					
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if (txtB.getText().isEmpty())
+					txtB.setText("0");
 			}
 		});
+
+	}
+	
+	private BufferedImage doTheIntpainting() throws IOException {
+		Matrix matrix = new Matrix(txtLienImage.getText());
+		Matrix matrix2 = new Matrix(txtLienMasque.getText());
+		topologyV2.Color color = new topologyV2.Color(lblCouleur.getBackground().getBlue(),
+				lblCouleur.getBackground().getGreen(), lblCouleur.getBackground().getRed());
+		Mask mask = new Mask(matrix2, color);
+		Inpainting inpainting = new Inpainting(matrix, mask);
+		inpainting.restore(5, 5);
+		BufferedImage img = new BufferedImage(inpainting.image.width, inpainting.image.height,
+				BufferedImage.TYPE_3BYTE_BGR);
+		byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+		int k = 0;
+		for (int j = 0; j < matrix.height; j++)
+			for (int i = 0; i < matrix.width; i++)
+				for (int c = 0; c < 3; c++)
+					pixels[k++] = matrix.val[i][j].val[c];
 		
+		return img;
+	}
+	
+	private void setAllFalse() {
+		lblTitreImages.setVisible(false);
+		btnSearchImage.setVisible(false);
+		txtLienImage.setVisible(false);
+		lblImage.setVisible(false);
+		txtLienMasque.setVisible(false);
+		lblMasque.setVisible(false);
+		btnSearchMasque.setVisible(false);
+		image.setVisible(false);
+		masque.setVisible(false);
+		separator.setVisible(false);
+		lblTitreCouleur.setVisible(false);
+		lblCouleur.setVisible(false);
+		btnExec.setVisible(false);
+		btnColorPicker.setVisible(false);
+		txtR.setVisible(false);
+		txtG.setVisible(false);
+		txtB.setVisible(false);
+		btnApply.setVisible(false);
+		lblLoading.setVisible(false);
+	}
+	
+	
+	private void setAllTrue() {
+		lblTitreImages.setVisible(true);
+		btnSearchImage.setVisible(true);
+		txtLienImage.setVisible(true);
+		lblImage.setVisible(true);
+		txtLienMasque.setVisible(true);
+		lblMasque.setVisible(true);
+		btnSearchMasque.setVisible(true);
+		image.setVisible(true);
+		masque.setVisible(true);
+		separator.setVisible(true);
+		lblTitreCouleur.setVisible(true);
+		lblCouleur.setVisible(true);
+		btnExec.setVisible(true);
+		btnColorPicker.setVisible(true);
+		txtR.setVisible(true);
+		txtG.setVisible(true);
+		txtB.setVisible(true);
+		btnApply.setVisible(true);
 		
+		lblLoading.setVisible(false);
 	}
 }
