87f47d42987685b037a4a3da34de8c8d46de5ee6
Francois Rigaut <francois.rigaut@etu.unice.fr>
2017-12-09 20:03:06 +0100

    Correction touchedby

--- Int_Pingu/src/topologyV2/Mask.java
+++ Int_Pingu/src/topologyV2/Mask.java
@@ -15,67 +15,78 @@ public class Mask extends BoundingBox
 			@param point a  point of the BoundingBox
 			@return True if on the pixel that as point for a corner belongs to the mask (4 pixels at most)
 		*/
-		public boolean touchedBy(Point p){
-			if(p.onCorner()){
-				if(p.i == 0 && p.j == 0){
-					if(this.val[0][0])
-						return true;
-					return false;
-				}
-				else{
-					if(p.i == 0){
-						if(this.val[0][height])
-							return true;
-						return false;
-					}
-					else{
-						if(p.j == 0){
-							if(this.val[width][0])
-								return true;
-							return false;
-						}
-							else{
-								if(this.val[width][height])
-									return true;
-								return false;
-							}
-					}
-				}
-			}
-			else{
-				if(p.onBorder()){
-					if(p.i == 0){
-						if(val[0][p.j] || val[0][p.j-1])
-							return true;
-						return false;
-					}
-					else{
-						if(p.j == 0){
-							if(val[p.i][0] || val[p.j-1][0])
-								return true;
-							return false;
-						}
-						else{
-							if(p.j<p.i){
-								if(val[p.i-1][p.j] || val[p.i-1][p.j-1])
-									return true;
-								return false;
-							}
-							else{
-								if(val[p.i][p.j-1] || val[p.i-1][p.j-1])
-									return true;
-								return false;
-							}
-						}
-					}
-				}
-				else{
-					if(val[p.i][p.j] || val[p.i-1][p.j] || val[p.i][p.j-1] || val[p.i-1][p.j-1])
-						return true;
-					return false;
-				}
-			}
+		
+		//VPROF
+		public boolean touchedBy(Point point){
+			int i=point.i;int j=point.j;
+			boolean res=false;
+			for(int I=Math.max(i-1,0);I<Math.min(i+1,width);I++)
+				for(int J=Math.max(j-1,0);J<Math.min(j+1,height);J++)
+					res=res||val[I][J];
+			return res;
 		}
+		
+//		public boolean touchedBy(Point p){
+//			if(p.onCorner()){
+//				if(p.i == 0 && p.j == 0){
+//					if(this.val[0][0])
+//						return true;
+//					return false;
+//				}
+//				else{
+//					if(p.i == 0){
+//						if(this.val[0][height])
+//							return true;
+//						return false;
+//					}
+//					else{
+//						if(p.j == 0){
+//							if(this.val[width][0])
+//								return true;
+//							return false;
+//						}
+//							else{
+//								if(this.val[width][height])
+//									return true;
+//								return false;
+//							}
+//					}
+//				}
+//			}
+//			else{
+//				if(p.onBorder()){
+//					if(p.i == 0){
+//						if(val[0][p.j] || val[0][p.j-1])
+//							return true;
+//						return false;
+//					}
+//					else{
+//						if(p.j == 0){
+//							if(val[p.i][0] || val[p.j-1][0])
+//								return true;
+//							return false;
+//						}
+//						else{
+//							if(p.j<p.i){
+//								if(val[p.i-1][p.j] || val[p.i-1][p.j-1])
+//									return true;
+//								return false;
+//							}
+//							else{
+//								if(val[p.i][p.j-1] || val[p.i-1][p.j-1])
+//									return true;
+//								return false;
+//							}
+//						}
+//					}
+//				}
+//				else{
+//					if(val[p.i][p.j] || val[p.i-1][p.j] || val[p.i][p.j-1] || val[p.i-1][p.j-1])
+//						return true;
+//					return false;
+//				}
+//			}
+//		}
 		/**
 			@param matrix contains the image that defines the mask
 			@param color the color of the pixel belonging to the mask
